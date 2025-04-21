b79f1f1a7ee5efabf34d2c0b43447b3297837e91
Luke44565 <luke.j.levine@gmail.com>
2025-04-21 16:54:31 -0400

    remove unused dep

--- django-app/hockeydb/views.py
+++ django-app/hockeydb/views.py
@@ -7,9 +7,6 @@ import json
 
 logger = logging.getLogger(__name__)
 
-def home(request):
-    return JsonResponse({"status":"success","message": "API Online"})
-
 def get_player(request):
     return Player.api_read(request)
 
