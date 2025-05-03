af63938a232defa9baacbabd66c68e96584437bd
Mark Santa Clara Munro <mark@munro.pt>
2025-05-03 17:20:13 -0400

    Create all needed functionality for the team view

--- frontend/src/views/teams-view.vue
+++ frontend/src/views/teams-view.vue
@@ -1,129 +1,223 @@
 <template>
 
-    <div>
-        <select v-model="selectedTeamId" class="filter-select" @change="selectTeam()">
-          <option value="">Add Team</option>
-          <option v-for="t in teams" :key="t.id" :value="t.id">
-            {{ t.name }}
-          </option>
-        </select>
-    </div>
-    <div>
+  <div>
+      <select v-model="selectedTeamId" class="filter-select" @change="selectTeam()">
+        <option value="">Add Team</option>
+        <option v-for="t in teams" :key="t.id" :value="t.id">
+          {{ t.name }}
+        </option>
+      </select>
+  </div>
+  <div>
 
-    </div>
+  </div>
 
-    <div class="details-grid">
-      <div class="detail-item">
-        <span class="detail-label">Team Name:</span>
-        <span class="detail-value"><input type="text" v-model="team.name"/></span>
-      </div>
-      <div class="detail-item">
-        <span class="detail-label">Coach Name:</span>
-        <span class="detail-value"><input type="text" v-model="team.coach_first_name" placeholder="First"/><input type="text" v-model="team.coach_last_name" placeholder="Last"/></span>
-      </div>
+  <div class="details-grid">
+    <div class="detail-item">
+      <span class="detail-label">Team Name:</span>
+      <span class="detail-value"><input type="text" v-model="team.name"/></span>
     </div>
-    <button v-if="team.name.length > 3" @click="saveTeam(team)">Save</button>    
-  </template>
-  
-  <script>
-  export default {
-    name: 'TeamView',
-    data() {
-      return {
-        teams: [
-          {id:1, name:"Team A", coach_first_name:"Coach", coach_last_name:"1"},
-          {id:2, name:"Team B", coach_first_name:"Coach", coach_last_name:"2"},
-          {id:3, name:"Team C", coach_first_name:"Coach", coach_last_name:"3"},
-        ],
-        team: {id:0, name:"", coach_first_name:"", coach_last_name:""},
-        selectedTeamId: ''
+    <div class="detail-item">
+      <span class="detail-label">Coach Name:</span>
+      <span class="detail-value"><input type="text" v-model="team.coach_first_name" placeholder="First"/><input type="text" v-model="team.coach_last_name" placeholder="Last"/></span>
+    </div>
+  </div>
+  <button v-if="team.name.length > 3" @click="saveTeam(team)">Save</button>    
+
+  <h3 v-if="team.players">Players</h3>
+  <div class="details-grid" v-for="player in team.players" :key="player.name">
+    <div class="detail-item">
+      <span class="detail-label">Name:</span>
+      <span class="detail-value">{{ player.first_name }} {{ player.last_name }}</span>
+    </div>
+    <div class="detail-item">
+      <span class="detail-label">Number:</span>
+      <span class="detail-value"><input type="text" v-model="player.number_on_team" placeholder=""/></span>
+      <button @click="saveTeamPlayer(team.id, player)">Save</button>  
+      <button @click="removeTeamPlayer(team.id, player)">Remove</button>  
+    </div>
+  </div>
+
+  <div v-if="team.id">
+  <h3>Add Player</h3>
+  <div class="details-grid">
+    <div class="detail-item">
+      <span class="detail-label">Name:</span>
+      <span class="detail-value">
+        <select v-model="addPlayer.id" class="filter-select">
+        <option value="0" >Select Player</option>
+        <option v-for="p in players" :key="p.id" :value="p.id">
+          {{ p.first_name + ' ' + p.last_name }}
+        </option>
+      </select>
+      </span>
+    </div>
+    <div class="detail-item">
+      <span class="detail-label">Number:</span>
+      <span class="detail-value"><input type="number" v-model="addPlayer.number_on_team" placeholder=""/></span>
+      <button @click="addTeamPlayer(team.id, addPlayer)">Add</button>  
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'TeamView',
+  data() {
+    return {
+      teams: [
+        {id:1, name:"Team A", coach_first_name:"Coach", coach_last_name:"1", players:[
+        { id: 1, first_name: 'Player 1', last_name: '', number_on_team: 5, position: 'Forward', grad_year: 2025},
+        { id: 2, first_name: 'Player 2', last_name: '', number_on_team: 28, position: 'Defense', grad_year: 2026},
+      ]},
+        {id:2, name:"Team B", coach_first_name:"Coach", coach_last_name:"2", players:[
+        { id: 1, first_name: 'Player 3', last_name: '', number_on_team: 5, position: 'Forward', grad_year: 2025},
+        { id: 2, first_name: 'Player 4', last_name: '', number_on_team: 28, position: 'Defense', grad_year: 2026},
+      ]},
+        {id:3, name:"Team C", coach_first_name:"Coach", coach_last_name:"3", players:[
+        { id: 1, first_name: 'Player 5', last_name: '', number_on_team: 5, position: 'Forward', grad_year: 2025},
+        { id: 2, first_name: 'Player 6', last_name: '', number_on_team: 28, position: 'Defense', grad_year: 2026},
+      ]},
+      ],
+      team: {id:0, name:"", coach_first_name:"", coach_last_name:""},
+      selectedTeamId: '',
+      players: [
+        { id: 1, first_name: 'Player 1', last_name: '', number_on_team: 5, position: 'Forward', grad_year: 2025},
+        { id: 2, first_name: 'Player 2', last_name: '', number_on_team: 28, position: 'Defense', grad_year: 2026},
+      ],
+      addPlayer: { id: 0, number_on_team: ''},
+    }
+  },
+
+  created() {
+    // fetch on init
+    this.fetchTeams();
+    this.fetchPlayers();
+  },
+
+  methods:{
+    selectTeam(){
+      if (this.selectedTeamId > 0) this.team = this.teams[this.selectedTeamId-1];
+      else this.team = {id:0, name:"", coach_first_name:"", coach_last_name:""};
+    },
+
+    async fetchTeams() {
+      const url = 'http://localhost/api/search/team?all';
+      const response = await (await fetch(url)).json();
+      console.log('Teams response', response);
+      this.teams = response.data;
+    },
+
+    async fetchPlayers() {
+      const url = 'http://localhost/api/search/player?all';
+      const response = await (await fetch(url)).json();
+      console.log('Players response', response);
+      this.players = response.data;
+    },
+
+    async saveTeamPlayer(team_id, player){
+      if (!player.id || !player.number_on_team) return;
+      const body = JSON.stringify({team_id: team_id, player_id: player.id, number_on_team: player.number_on_team});
+      console.log(body);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: body
+      };
+      const response = await fetch("http://localhost/api/update/team_membership", requestOptions);
+      const data = await response.json();      
+      if (data.data.id) {
+        console.log("Team Membership saved id", data.data.id);   
+      } else {
+        alert("Error saving team membership");
       }
     },
 
-    created() {
-      // fetch on init
-      this.fetchTeams();
+    async removeTeamPlayer(team_id, player){
+      const body = JSON.stringify({team_id: team_id, player_id: player.id});
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: body
+      };
+      const response = await fetch("http://localhost/api/delete/team_membership", requestOptions);
+      const data = await response.json();      
+      if (data.success) {
+        console.log("Team Membership deleted");  
+        this.fetchTeams(); //refresh list
+      } else {
+        alert("Error deleting team membership");
+      }
     },
 
-    methods:{
-      selectTeam(){
-        if (this.selectedTeamId > 0) this.team = this.teams[this.selectedTeamId-1];
-        else this.team = {id:0, name:"", coach_first_name:"", coach_last_name:""};
-      },
-      async fetchTeams() {
-        const url = 'http://localhost/api/search/team?all';
-        const response = await (await fetch(url)).json();
-        console.log('Teams response', response);
-        this.teams = response.data;
-      },
-      async saveTeam(team){
-        const body = JSON.stringify(team);
-        console.log(body);
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: body
-        };
-        var mode = "create";
-        if (team.id) mode = "update"; 
-        const response = await fetch("http://localhost/api/"+mode+"/team", requestOptions);
-        const data = await response.json();      
-        if (data.data.id) {
-          console.log("Team saved id", data.data.id);   
-          this.fetchTeams(); //refresh listbox
-          this.selectedTeamId = data.data.id;
-        } else {
-          alert("Error saving team");
-        }
+    async addTeamPlayer(team_id, player){
+      if (!player.id || !player.number_on_team) return;
+      const body = JSON.stringify({team_id: team_id, player_id: player.id, number_on_team: player.number_on_team});
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: body
+      };
+      const response = await fetch("http://localhost/api/create/team_membership", requestOptions);
+      const data = await response.json();      
+      if (data.data.id) {
+        console.log("Team Membership added id", data.data.id);   
+        this.fetchTeams(); //refresh list
+      } else {
+        alert("Error adding team membership");
       }
     }
-  };
-  </script>
-  
-  <style> 
-  .filters {
-    display: flex;
-    gap: 10px;
-    margin-bottom: 20px;
   }
+};
+</script>
 
-  .filter-input,
-  .filter-select {
-    padding: 5px;
-    font-size: 1rem;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-  }
-  .details-grid {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 15px;
-  margin-bottom: 30px;
-  padding: 20px;
-  background: #01463c;
-  border-radius: 8px;
+<style> 
+.filters {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.filter-input,
+.filter-select {
+  padding: 5px;
+  font-size: 1rem;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+.details-grid {
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 15px;
+margin-bottom: 30px;
+padding: 20px;
+background: #01463c;
+border-radius: 8px;
 }
 
 .detail-item {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 12px;
-  background: #006d5b;
-  border-radius: 6px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 12px;
+background: #006d5b;
+border-radius: 6px;
+color: white;
 }  
 
 .detail-item:nth-child(odd) {
-  background: #005c4d;
+background: #005c4d;
 }
 
 .detail-label {
-  font-weight: 600;
-  color: #ffcd00;
+font-weight: 600;
+color: #ffcd00;
 }
 
 .detail-value {
-  text-align: right;
+text-align: right;
 }
 
 </style>
